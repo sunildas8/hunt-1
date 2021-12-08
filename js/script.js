97416f1252fea928192131e5160de28860241f35
sunildas1 <95643764+sunildas1@users.noreply.github.com>
2021-12-08 13:25:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+$('.venobox').venobox();
+
+$(".service-slider").slick({
+    vertical: true,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "0",
+    prevArrow: '<i class="fas fa-chevron-up prev"></i>',
+    nextArrow: '<i class="fas fa-chevron-down next"></i>',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            vertical: false,
+          }
+        }
+      ]
+})
+
+$(".service-slider-2").slick({
+    vertical: true,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "0",
+    prevArrow: '<i class="fas fa-chevron-up prev"></i>',
+    nextArrow: '<i class="fas fa-chevron-down next"></i>',
+    asNavFor: ".details-slider",
+    
+})
+
+$(".details-slider").slick({
+    vertical: true,
+    slidesToShow: 1,
+    arrows: false,
+    asNavFor: ".service-slider-2",
+})
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+});
+
+$(".marketplace-slider").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    centerMode: true,
+    centerPadding: "0",
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+})
